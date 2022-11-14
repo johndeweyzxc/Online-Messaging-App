e607237f3ebff7a1e6c9213a1f3e2a0613611650
johndeweyzxc <johndewey02003@gmail.com>
2022-11-14 10:01:21 +0800

    Created messages.css for storing utility classes

--- src/MessageContent.js
+++ src/MessageContent.js
@@ -1,11 +1,8 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 
-export default function MessageContent({
-  Name,
-  Messages,
-  setMessages,
-  setUserLogs,
-}) {
+import "./Style/messages.css";
+
+export default function MessageContent({ Name, Messages, setMessages, setUserLogs }) {
   const inputRef = useRef();
 
   // Send a message to the server
@@ -19,19 +16,16 @@ export default function MessageContent({
 
     if (val !== "") {
       try {
-        response = await fetch(
-          "https://online-messaging-api.vercel.app/SendMessage",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              UserName: Name,
-              MessageContent: val,
-            }),
-          }
-        );
+        response = await fetch("https://online-messaging-api.vercel.app/SendMessage", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            UserName: Name,
+            MessageContent: val,
+          }),
+        });
       } catch (error) {
         console.log(error);
       }
@@ -57,22 +51,9 @@ export default function MessageContent({
   // Where the user create and sends its message
   const InputMessage = () => {
     return (
-      <div className='flex w-full'>
-        <input
-          className='
-          p-2 pl-4 pr-4 text-lg Quicksand outline-none w-full rounded-md shadow-lg
-          border-2 border-solid border-white focus:border-CoolBlue'
-          placeholder={"Send a message"}
-          ref={inputRef}
-          type={"text"}
-        />
-        <div
-          onClick={SendMessage}
-          className='
-        flex justify-center items-center p-2 pl-4 pr-4 text-lg bg-CoolBlue text-white
-        ml-6 rounded-md shadow-lg tracking-wide font-semibold cursor-pointer
-        hover:bg-blue-500 transition-all ease-in'
-        >
+      <div className="flex w-full">
+        <input className="InputMessage" placeholder={"Send a message"} ref={inputRef} type={"text"} />
+        <div onClick={SendMessage} className="Send">
           Send
         </div>
       </div>
@@ -82,11 +63,9 @@ export default function MessageContent({
   // The indivual message inside the message view
   const IndividualMessage = (msg) => {
     return (
-      <div key={msg.id} className='mb-4 w-max'>
-        <div className='text-xs text-slate-400 tracking-wider pl-2 mb-1'>
-          {msg.creator}
-        </div>
-        <div className='bg-slate-200 p-2 pl-4 pr-4 rounded-full'>{msg.msg}</div>
+      <div key={msg.id} className="mb-4 w-max">
+        <div className="text-xs text-slate-400 tracking-wider pl-2 mb-1">{msg.creator}</div>
+        <div className="bg-slate-200 p-2 pl-4 pr-4 rounded-full">{msg.msg}</div>
       </div>
     );
   };
@@ -94,9 +73,9 @@ export default function MessageContent({
   // This is the list of the message
   const MessageView = () => {
     return (
-      <div className='w-full h-full flex flex-col'>
-        <div className='flex-grow mb-4'>
-          <div className='bg-white h-full mb-4 rounded-xl shadow-lg p-4'>
+      <div className="w-full h-full flex flex-col">
+        <div className="flex-grow mb-4">
+          <div className="bg-white h-full mb-4 rounded-xl shadow-lg p-4">
             {Messages.map(IndividualMessage)}
           </div>
         </div>
