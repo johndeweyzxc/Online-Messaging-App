4f8a762ca25ad96e8a4cd820651741bb99fbd47f
johndeweyzxc <johndewey02003@gmail.com>
2022-11-22 18:09:48 +0800

    Added supported screen for tablet and phone

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       colors: {
         CoolBlue: "#00a2ff",
       },
+      screens: {
+        tablet: { max: "820px" },
+        phone: { max: "420px" },
+      },
       fontFamily: {
         Quicksand: ["Quicksand", "sans-serif"],
       },
