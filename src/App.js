21b96e417e0c2bf449ec2afe3630ac1fabb825c7
johndeweyzxc <johndewey02003@gmail.com>
2022-11-22 18:31:32 +0800

    Added responsive design for tablet and phone screen

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 import Activity from "./Activity";
 import MessageContent from "./MessageContent";
+import SideBarActivity from "./SideBarActivity";
 
 function App() {
   const [Name, setName] = useState(null);
   const [UserLogs, setUserLogs] = useState([]);
   const [Messages, setMessages] = useState([]);
+  const [OpenNav, setOpenNav] = useState(false);
 
   // Join to the server
   const JoinServer = async (Value) => {
     let response;
-
-    // Cloud API https://online-messaging-api.vercel.app/
-    // Localhost http://localhost:4000
 
     try {
       response = await fetch("https://online-messaging-api.vercel.app/Join", {
@@ -50,25 +50,36 @@ function App() {
 
   // The user is new to the app and has not joined the server before.
   if (Name === null && !localStorage.getItem("localName")) {
-    let name = prompt(
-      "Enter your name (Must be 3 to 10 characters with no spaces)"
-    );
+    let name = prompt("Enter your name (Must be 3 to 10 characters with no spaces)");
     JoinServer(name);
   } else {
     return (
-      <div className='w-screen h-screen bg-slate-200 flex p-6'>
-        <Activity
+      <div className="h-screen w-screen flex flex-col">
+        <header className="bg-CoolBlue hidden tablet:block p-3 z-10">
+          <GiHamburgerMenu className="text-4xl text-white" onClick={() => setOpenNav(true)} />
+        </header>
+        <SideBarActivity
           UserLogs={UserLogs}
           setUserLogs={setUserLogs}
           Messages={Messages}
           setMessages={setMessages}
+          OpenNav={OpenNav}
+          setOpenNav={setOpenNav}
         />
-        <MessageContent
-          Name={Name}
-          Messages={Messages}
-          setMessages={setMessages}
-          setUserLogs={setUserLogs}
-        />
+        <div className={`flex p-6 h-full bg-slate-200 phone:p-4 tablet:${OpenNav ? "z-0" : "z-10"}`}>
+          <Activity
+            UserLogs={UserLogs}
+            setUserLogs={setUserLogs}
+            Messages={Messages}
+            setMessages={setMessages}
+          />
+          <MessageContent
+            Name={Name}
+            Messages={Messages}
+            setMessages={setMessages}
+            setUserLogs={setUserLogs}
+          />
+        </div>
       </div>
     );
   }
---
+++ src/SideBarActivity.js
@@ -0,0 +1,98 @@
+import React from "react";
+import "./Style/activity.css";
+import { GrClose } from "react-icons/gr";
+
+function SideBarActivity({ UserLogs, setUserLogs, setMessages, Messages, OpenNav, setOpenNav }) {
+  // Sync data from the server
+  const ServerSync = async () => {
+    let response;
+
+    let lastUserLog = UserLogs.length === 0 ? "Empty" : UserLogs[UserLogs.length - 1].id;
+    let lastMsg = Messages.length === 0 ? "Empty" : Messages[Messages.length - 1].id;
+
+    try {
+      response = await fetch("https://online-messaging-api.vercel.app/GetUpdates", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          LatestMsgId: lastMsg,
+          LActivityId: lastUserLog,
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    if (response) {
+      const result = await response.json();
+      let UpdatedAct = result.ActivityLog;
+      let UpdatedMsg = result.SentMessages;
+
+      if (UpdatedAct === undefined) {
+        window.location.reload(false);
+      } else {
+        setUserLogs((prev) => {
+          return [...prev, ...UpdatedAct];
+        });
+      }
+
+      if (UpdatedMsg === undefined) {
+        window.location.reload(false);
+      } else {
+        setMessages((prev) => {
+          return [...prev, ...UpdatedMsg];
+        });
+      }
+    }
+  };
+
+  // Return this if there is no activity yet :(
+  const NoActivity = () => {
+    return (
+      <div className="text-sm tracking-wide text-slate-400">
+        There is no activity yet, invite your friends!
+      </div>
+    );
+  };
+
+  // Component for individual activity
+  const IndividualActivity = (log) => {
+    return (
+      <div className="font-Quicksand" key={log.id}>
+        <div className="text-base font-medium mb-2 bg-slate-200 p-2 pl-4 pr-4 rounded-md">{log.msg}</div>
+      </div>
+    );
+  };
+
+  return (
+    <div className={`h-screen w-screen absolute hidden tablet:flex ${OpenNav ? "z-10" : "z-0"}`}>
+      <div
+        className={`bg-white shadow-2xl border-r-[1px] border-solid 
+        border-slate-400 transition-all duration-300 ease-in-out
+        ${OpenNav ? "translate-x-0" : "-translate-x-full"}`}
+      >
+        <div className="flex flex-col h-full">
+          <div className="flex justify-end m-2">
+            <GrClose className="text-4xl" onClick={() => setOpenNav(false)} />
+          </div>
+
+          <div className="mb-4 bg-white p-2 rounded-xl h-full">
+            <div className="Activities">{`${UserLogs.length} Activities`}</div>
+            <div className="w-full h-[1px] bg-slate-400 mb-4 mt-1" />
+            <div className="mb-2 overflow-auto">
+              {UserLogs.length === 0 ? <NoActivity /> : UserLogs.map(IndividualActivity)}
+            </div>
+          </div>
+
+          <div className="SyncServer" onClick={ServerSync}>
+            Sync with Server
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SideBarActivity;
